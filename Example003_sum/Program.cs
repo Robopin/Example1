be6e95b58eae101610c5d7b574ea09760330e9c7
Robopin <vlasizov@yandex.ru>
2022-11-22 15:51:55 +0300

    12

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); //случайное число 1 - 9
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
 
 
