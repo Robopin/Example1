97d442792ac8020d75250e41ffcc66bf4d77a80a
Robopin <vlasizov@yandex.ru>
2022-11-23 18:33:53 +0300

    Add Example 6

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,28 @@
+﻿int a = 1;
+int b = 10;
+int c = 4;
+int d = 6;
+int e = 4;
+int maxnumber = a;
+if (a > maxnumber)
+{
+    maxnumber = a;
+}
+if (b > maxnumber)
+{
+    maxnumber = b;
+}
+if (c > maxnumber)
+{
+    maxnumber = c;
+}
+if (d > maxnumber)
+{
+    maxnumber = d;
+}
+if (e > maxnumber)
+{
+    maxnumber = e;
+}
+Console.Write("Максимальное число: ");
+Console.WriteLine(maxnumber);
