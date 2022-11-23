07b7af982675c07cbcff96513a522743650bf159
Robopin <vlasizov@yandex.ru>
2022-11-24 00:44:32 +0300

    Add Example 10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int []array = {11, 32, 45, 23, 4, 23, 54, 6, 7, 5};
+int n = array.Length;
+int find = 23;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
