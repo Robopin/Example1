2f33a58484fcffc9f5e179cc7c30e86ba532be59
Robopin <vlasizov@yandex.ru>
2022-11-23 18:20:31 +0300

    add Example 5

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Put your name: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else 
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
